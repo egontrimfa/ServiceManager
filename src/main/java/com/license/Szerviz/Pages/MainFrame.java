8c84e27dbfa4e40313bf484b851872d5957d3d68
Alexers <egontrimfa@gmail.com>
2020-02-02 12:51:54 +0200

    QuickSearch, Column Sorting, Company Info, Update and Delete on ClientList

--- src/main/java/com/license/Szerviz/Pages/MainFrame.java
+++ src/main/java/com/license/Szerviz/Pages/MainFrame.java
@@ -15,12 +15,17 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.RowFilter;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import com.license.HibernateUtil.HibernateUtil;
 import com.license.Szerviz.Entities.Client;
@@ -32,9 +37,13 @@ import javax.swing.JButton;
 
 import java.awt.Cursor;
 import java.awt.CardLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class MainFrame extends JFrame {
 
+	static MainFrame mainFrame = new MainFrame();
+	
 	private JPanel contentPane;
 	static private JPanel panelDB;
 	static private JPanel panelCR;
@@ -42,7 +51,11 @@ public class MainFrame extends JFrame {
 	static private JPanel panelNPR;
 	static private JPanel panelCL;
 	
-	private static JTable table;
+	static private TableRowSorter<TableModel> rowSorter;
+	private Client selectedClient;
+	private Company selectedCompany;
+	
+	private static JTable clientsTable;
 	private JTextField clientName;
 	private JTextField clientPhone;
 	private JTextField isCompany;
@@ -60,7 +73,26 @@ public class MainFrame extends JFrame {
 	private JTextField txtNumarulDeTelefonN;
 	
 	int xx,xy;
-	static MainFrame mainFrame = new MainFrame();
+	private int selectedID;
+	
+	private JTextField textFilter;
+	private JTextField companyName;
+	private JTextField companyAddress;
+	private JTextField companyPhone;
+	private JTextField cif;
+	private JTextField regNR;
+	private JTextField bank;
+	private JTextField iban;
+	private JTextField branchOffice;
+	
+	private JLabel companyNameInfo;
+	private JLabel companyAddressInfo;
+	private JLabel companyPhoneInfo;
+	private JLabel cifInfo;
+	private JLabel regNRInfo;
+	private JLabel bankInfo;
+	private JLabel ibanInfo;
+	private JLabel branchInfo;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -465,27 +497,50 @@ public class MainFrame extends JFrame {
 		panelCL.setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(350, 97, 945, 488);
+		scrollPane.setBounds(436, 140, 849, 445);
 		panelCL.add(scrollPane);
 		
-		table = new JTable();
-		table.addMouseListener(new MouseAdapter() {
+		clientsTable = new JTable();
+		clientsTable.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				int index = table.getSelectedRow();
-				TableModel model = table.getModel();
-				clientName.setText(model.getValueAt(index, 0).toString());
-				clientPhone.setText(model.getValueAt(index, 1).toString());
-				isCompany.setText(model.getValueAt(index, 2).toString());
+				int index = clientsTable.getSelectedRow();
+				TableModel model = clientsTable.getModel();
+				clientName.setText(model.getValueAt(index, 1).toString());
+				clientPhone.setText(model.getValueAt(index, 2).toString());
+				isCompany.setText(model.getValueAt(index, 3).toString());
+				selectedID = Integer.parseInt(model.getValueAt(index, 0).toString());
+				
+				if(isCompany.getText().equals("true")) {
+					//setting company parameters to visible and empty, and labels to visible as well					
+					companyDataSetter(true);
+					
+					//Getting the selected company by ID
+					getClientByID(selectedID);
+					selectedCompany = selectedClient.getCompany();
+					
+					//Load data into the company's parameters					
+					companyName.setText(selectedCompany.getCompanyname());
+					companyAddress.setText(selectedCompany.getCompanyaddress());
+					companyPhone.setText(selectedCompany.getCompanyphone());
+					cif.setText(selectedCompany.getCif());
+					regNR.setText(selectedCompany.getRegnr());
+					bank.setText(selectedCompany.getBank());
+					iban.setText(selectedCompany.getIban());
+					branchOffice.setText(selectedCompany.getBranchoffice());
+					
+				}else {
+					companyDataSetter(false);
+				}
 			}
 		});
-		table.setRowHeight(30);
-		table.getTableHeader().setFont(new Font("Tahoma", Font.BOLD, 21));
-		table.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		scrollPane.setViewportView(table);
+		clientsTable.setRowHeight(30);
+		clientsTable.getTableHeader().setFont(new Font("Tahoma", Font.BOLD, 21));
+		clientsTable.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		scrollPane.setViewportView(clientsTable);
 		
 		JPanel infoPanel = new JPanel();
-		infoPanel.setBounds(12, 97, 326, 488);
+		infoPanel.setBounds(12, 97, 412, 488);
 		panelCL.add(infoPanel);
 		infoPanel.setLayout(null);
 		
@@ -499,7 +554,7 @@ public class MainFrame extends JFrame {
 		infoPanel.add(lblDetaliiClientului);
 		
 		clientName = new JTextField();
-		clientName.setBounds(131, 53, 183, 22);
+		clientName.setBounds(131, 53, 269, 22);
 		infoPanel.add(clientName);
 		clientName.setColumns(10);
 		
@@ -508,7 +563,7 @@ public class MainFrame extends JFrame {
 		infoPanel.add(lblNumarulDeTelefonInfo);
 		
 		clientPhone = new JTextField();
-		clientPhone.setBounds(131, 82, 183, 22);
+		clientPhone.setBounds(131, 82, 269, 22);
 		infoPanel.add(clientPhone);
 		clientPhone.setColumns(10);
 		
@@ -517,9 +572,131 @@ public class MainFrame extends JFrame {
 		infoPanel.add(lblStatutulClientului);
 		
 		isCompany = new JTextField();
-		isCompany.setBounds(131, 111, 183, 22);
+		isCompany.setEditable(false);
+		isCompany.setBounds(131, 111, 269, 22);
 		infoPanel.add(isCompany);
 		isCompany.setColumns(10);
+		
+		companyName = new JTextField();
+		companyName.setVisible(false);
+		companyName.setBounds(131, 146, 269, 22);
+		infoPanel.add(companyName);
+		companyName.setColumns(10);
+		
+		companyAddress = new JTextField();
+		companyAddress.setVisible(false);
+		companyAddress.setColumns(10);
+		companyAddress.setBounds(131, 181, 269, 22);
+		infoPanel.add(companyAddress);
+		
+		companyPhone = new JTextField();
+		companyPhone.setVisible(false);
+		companyPhone.setColumns(10);
+		companyPhone.setBounds(131, 216, 269, 22);
+		infoPanel.add(companyPhone);
+		
+		cif = new JTextField();
+		cif.setVisible(false);
+		cif.setColumns(10);
+		cif.setBounds(131, 251, 269, 22);
+		infoPanel.add(cif);
+		
+		regNR = new JTextField();
+		regNR.setVisible(false);
+		regNR.setColumns(10);
+		regNR.setBounds(131, 286, 269, 22);
+		infoPanel.add(regNR);
+		
+		bank = new JTextField();
+		bank.setVisible(false);
+		bank.setColumns(10);
+		bank.setBounds(131, 321, 269, 22);
+		infoPanel.add(bank);
+		
+		iban = new JTextField();
+		iban.setVisible(false);
+		iban.setColumns(10);
+		iban.setBounds(131, 356, 269, 22);
+		infoPanel.add(iban);
+		
+		branchOffice = new JTextField();
+		branchOffice.setVisible(false);
+		branchOffice.setColumns(10);
+		branchOffice.setBounds(131, 391, 269, 22);
+		infoPanel.add(branchOffice);
+		
+		companyNameInfo = new JLabel("Numele companiei:");
+		companyNameInfo.setVisible(false);
+		companyNameInfo.setBounds(12, 149, 116, 16);
+		infoPanel.add(companyNameInfo);
+		
+		companyAddressInfo = new JLabel("Addresa companiei:");
+		companyAddressInfo.setVisible(false);
+		companyAddressInfo.setBounds(12, 184, 116, 16);
+		infoPanel.add(companyAddressInfo);
+		
+		companyPhoneInfo = new JLabel("Num. tel. comp.:");
+		companyPhoneInfo.setVisible(false);
+		companyPhoneInfo.setBounds(12, 219, 103, 16);
+		infoPanel.add(companyPhoneInfo);
+		
+		cifInfo = new JLabel("CIF:");
+		cifInfo.setVisible(false);
+		cifInfo.setBounds(12, 254, 103, 16);
+		infoPanel.add(cifInfo);
+		
+		regNRInfo = new JLabel("NR de irgesitrare:");
+		regNRInfo.setVisible(false);
+		regNRInfo.setBounds(12, 289, 103, 16);
+		infoPanel.add(regNRInfo);
+		
+		bankInfo = new JLabel("Bank:");
+		bankInfo.setVisible(false);
+		bankInfo.setBounds(12, 324, 103, 16);
+		infoPanel.add(bankInfo);
+		
+		ibanInfo = new JLabel("IBAN:");
+		ibanInfo.setVisible(false);
+		ibanInfo.setBounds(12, 359, 103, 16);
+		infoPanel.add(ibanInfo);
+		
+		branchInfo = new JLabel("Branch office:");
+		branchInfo.setVisible(false);
+		branchInfo.setBounds(12, 394, 103, 16);
+		infoPanel.add(branchInfo);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(selectedID>0) {
+					if(isCompany.getText().equals("true")) {
+						updateLegalClient(selectedID, clientName.getText(), clientPhone.getText(), companyName.getText(), companyAddress.getText(), 
+								companyPhone.getText(), cif.getText(), regNR.getText(), bank.getText(), iban.getText(), branchOffice.getText());
+					}else {
+						updateNaturalClient(selectedID, clientName.getText(), clientPhone.getText());
+					}
+				}else {
+					System.out.println("You have to select a client first!");
+				}
+			}
+		});
+		btnUpdate.setBounds(69, 426, 116, 37);
+		infoPanel.add(btnUpdate);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(selectedID>0) {
+					deleteClient(selectedID);
+				}else {
+					System.out.println("You have to select a client first!");
+				}
+			}
+		});
+		btnDelete.setBounds(239, 426, 116, 37);
+		infoPanel.add(btnDelete);
 		
 		JLabel exitCL = new JLabel("");
 		exitCL.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -563,6 +740,32 @@ public class MainFrame extends JFrame {
 		reloadTable.setBounds(1179, 13, 45, 32);
 		panelCL.add(reloadTable);
 		
+		JPanel searchPanel = new JPanel();
+		searchPanel.setBounds(436, 97, 849, 37);
+		panelCL.add(searchPanel);
+		searchPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Căutare:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel.setBounds(12, 13, 60, 16);
+		searchPanel.add(lblNewLabel);
+		
+		textFilter = new JTextField();
+		
+		//Quick search method
+		
+		textFilter.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				String querry = textFilter.getText();
+				filterClients(querry);
+			}
+		});
+		
+		textFilter.setBounds(84, 11, 753, 22);
+		searchPanel.add(textFilter);
+		textFilter.setColumns(10);
+		
 		//On Creation
 		panelDB.setVisible(true);
 		panelCR.setVisible(false);
@@ -583,17 +786,19 @@ public class MainFrame extends JFrame {
 		session.beginTransaction();
 	    
 		List<Client> clients= (List<Client>)session.createQuery("from Client").list();
-		String[] cols= {"Numele clientului","Firm?","Numarul de telefon"};
+		String[] cols= {"ID", "Numele clientului", "Numarul de telefon", "Firm?"};
 		
 		DefaultTableModel dtm = new DefaultTableModel(cols, 0);
 		
 		for(Client c:clients) 
 		{
-		    String[] row= {c.getContactname(), String.valueOf(c.getIscompany()), c.getContactphone()};
+		    String[] row= {String.valueOf(c.getId()) ,c.getContactname(), c.getContactphone(), String.valueOf(c.getIscompany())};
 		    dtm.addRow( row );
 		}
 
-		table.setModel(dtm);
+		clientsTable.setModel(dtm);
+		clientsTable.removeColumn(clientsTable.getColumnModel().getColumn(0));
+		clientsTable.setAutoCreateRowSorter(true);
 	      
 	    //Committing the transaction
 	    session.getTransaction().commit();
@@ -664,6 +869,141 @@ public class MainFrame extends JFrame {
 	      
 	    //Committing the transaction
 	    session.getTransaction().commit();
+	    
+	    //Closing the session
+	    session.close();
+	    
+	    //Shotting down the session factory
+	    //HibernateUtil.shutDown();
+	}
+
+	private void filterClients(String querry) {
+		rowSorter = new TableRowSorter<TableModel>(clientsTable.getModel());
+		
+		clientsTable.setRowSorter(rowSorter);
+		
+		rowSorter.setRowFilter(RowFilter.regexFilter("(?i)" + querry));
+	}
+
+	private void getClientByID(int id) {	
+		//Creating session
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		//Begin transaction
+		session.beginTransaction();
+	    
+		//Client client = (Client) session.load(Client.class, new Integer(id));
+		
+		Query<Client> querry = session.createQuery("from Client where id=:id");
+		querry.setParameter("id", id);
+		selectedClient = (Client) querry.uniqueResult();
+	      
+	    //Committing the transaction
+	    session.getTransaction().commit();
+	    
+	    //Closing the session
+	    session.close();
+	    
+	    //Shotting down the session factory
+	    //HibernateUtil.shutDown();
+	}
+
+	private void companyDataSetter(Boolean state) {
+		companyName.setVisible(state);
+		companyName.setText(null);
+		companyNameInfo.setVisible(state);
+		companyAddress.setVisible(state);
+		companyAddress.setText(null);
+		companyAddressInfo.setVisible(state);
+		companyPhone.setVisible(state);
+		companyPhone.setText(null);
+		companyPhoneInfo.setVisible(state);
+		cif.setVisible(state);
+		cif.setText(null);
+		cifInfo.setVisible(state);
+		regNR.setVisible(state);
+		regNR.setText(null);
+		regNRInfo.setVisible(state);
+		bank.setVisible(state);
+		bank.setText(null);
+		bankInfo.setVisible(state);
+		iban.setVisible(state);
+		iban.setText(null);
+		ibanInfo.setVisible(state);
+		branchOffice.setVisible(state);
+		branchOffice.setText(null);
+		branchInfo.setVisible(state);
+	}
+
+	private void updateNaturalClient(int id, String cname, String cphone) {
+		//Creating session
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		//Begin transaction
+		session.beginTransaction();
+	    
+		Client client = (Client) session.get(Client.class, id);
+		client.setContactname(cname);
+		client.setContactphone(cphone);
+	      
+	    //Committing the transaction
+	    session.getTransaction().commit();
+	    
+	    System.out.println("Updated Successfully");
+	    
+	    //Closing the session
+	    session.close();
+	    
+	    //Shotting down the session factory
+	    //HibernateUtil.shutDown();
+	}
+	
+	private void updateLegalClient(int id, String cname, String cphone, String comname, String comadd, String comtel, String comcif, String comreg, String combank, String comiban, String combranch) {
+		//Creating session
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		//Begin transaction
+		session.beginTransaction();
+	    
+		Client client = (Client) session.get(Client.class, id);
+		client.setContactname(cname);
+		client.setContactphone(cphone);
+		
+		client.getCompany().setCompanyname(comname);
+		client.getCompany().setCompanyaddress(comadd);
+		client.getCompany().setCompanyphone(comtel);
+		client.getCompany().setCif(comcif);
+		client.getCompany().setRegnr(comreg);
+		client.getCompany().setBank(combank);
+		client.getCompany().setIban(comiban);
+		client.getCompany().setBranchoffice(combranch);
+	      
+	    //Committing the transaction
+	    session.getTransaction().commit();
+	    
+	    System.out.println("Updated Successfully");
+	    
+	    //Closing the session
+	    session.close();
+	    
+	    //Shotting down the session factory
+	    //HibernateUtil.shutDown();
+	}
+
+	private void deleteClient(int id) {
+		//Creating session
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		//Begin transaction
+		session.beginTransaction();
+	    
+		Client client = (Client)session.load(Client.class, id);
+		if(client.getCompany()!=null) {
+			session.remove(client.getCompany());
+		}
+		session.remove(client);
+		selectedID = 0;
+	      
+	    //Committing the transaction
+	    session.getTransaction().commit();
+	    
+	    System.out.println("Deleted Successfully");
 	    
 	    //Closing the session
 	    session.close();
